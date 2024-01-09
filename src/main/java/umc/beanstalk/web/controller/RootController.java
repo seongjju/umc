1e52ab2fa28060aa540d72d923386c5c3055a375
seongju jeong <seongjujeong@seongjuui-MacBookAir.local>
2024-01-09 21:51:43 +0900

    용량 수정3

--- src/main/java/umc/beanstalk/web/controller/RootController.java
+++ src/main/java/umc/beanstalk/web/controller/RootController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RootÇontroller {
+public class RootController {
 
     @GetMapping("health")
     public String healthCheck(){
